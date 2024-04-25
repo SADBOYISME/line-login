95ec52ccfb742563c7790011f027e2bfb125586d
Wis Chopper <wisarut0932473600@gmail.com>
2024-04-25 16:05:01 +0700

    comment error components

--- components/cilent/confetti.ts
+++ components/cilent/confetti.ts
@@ -1,10 +1,10 @@
-import React from "react";
-import useWindowSize from "react-use/lib/useWindowSize";
-import Confetti from "react-confetti";
+// import React from "react";
+// import useWindowSize from "react-use/lib/useWindowSize";
+// import Confetti from "react-confetti";
 
-const ConfettiComponent = () => {
-  const { width, height } = useWindowSize();
-  return <Confetti width={widthas} height={height} />;
-};
+// const ConfettiComponent = () => {
+//   const { width, height } = useWindowSize();
+//   return <Confetti width={widthas} height={height} />;
+// };
 
-export default ConfettiComponent;
+// export default ConfettiComponent;
